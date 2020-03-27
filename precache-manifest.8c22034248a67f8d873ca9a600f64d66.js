2744e0ac31b15c0491690583afd64475f86a08d5
Devildk384 <durgeshkumar384@gmail.com>
2020-03-27 20:58:13 +0530

    Updates

---
+++ precache-manifest.8c22034248a67f8d873ca9a600f64d66.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ae27d6c2a5a0f74f27f860322fb22be",
+    "url": "/emojipedia/index.html"
+  },
+  {
+    "revision": "bc447ffedee6b5531df8",
+    "url": "/emojipedia/static/js/2.a1b4c5f2.chunk.js"
+  },
+  {
+    "revision": "232112302770cdb6144c",
+    "url": "/emojipedia/static/js/main.03c5ec6d.chunk.js"
+  },
+  {
+    "revision": "359ecdf3bb61fa8a1538",
+    "url": "/emojipedia/static/js/runtime-main.492b4163.js"
+  }
+]);
